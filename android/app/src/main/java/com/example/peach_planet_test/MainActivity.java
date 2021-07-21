749047ebdf97775c62c0fde296a0d9c327c968b3
Inna <dsino@naver.com>
2021-07-22 02:43:38 +0900

    first commit

---
+++ android/app/src/main/java/com/example/peach_planet_test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.peach_planet_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/all.dart';
+import 'package:kakao_flutter_sdk/common.dart'; // import utility methods
+import 'package:kakao_flutter_sdk/user.dart';
+import 'constValue.dart';
+
+void main() {
+  KakaoContext.clientId = "2842cdeca515e53a5d1e7f1fdf72f23a";
+  KakaoContext.javascriptClientId = "080ef3c31fd3f162f8e3ddd5ef311260";
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: mainColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: KakaoLogin(),
+    );
+  }
+}
+
+class KakaoLogin extends StatefulWidget {
+  @override
+  _KakaoLoginState createState() => _KakaoLoginState();
+}
+
+class _KakaoLoginState extends State<KakaoLogin> {
+  bool _isKakaoTalkInstalled = false;
+  bool isLogin = false;
+
+  @override
+  void initState() {
+    _initKakaoTalkInstalled();
+    super.initState();
+  }
+
+  _initKakaoTalkInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    print('Kakao Install : ' + installed.toString());
+    //카카오톡 인스톨 여부에 따라 web연결 혹은 app 연결
+
+    setState(() {
+      _isKakaoTalkInstalled = installed;
+    });
+
+  }
+
+  _issueAccessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      print('token : ' + token.accessToken);
+      setState(() {
+        isLogin = true;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  _loginWithKakao() async {
+    try{
+      var code = await AuthCodeClient.instance.request();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  _loginWithTalk() async {
+    try{
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Center(
+        child: InkWell(
+            onTap: () => _isKakaoTalkInstalled ? _loginWithTalk : _loginWithKakao,
+            child:Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.1,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.yellow
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Icon(Icons.chat_bubble_rounded, color: Colors.brown[800]),
+                  SizedBox(width: 10,),
+                  Text(
+                    '카카오 계정으로 시작',
+                    style: TextStyle(
+                        color: Colors.brown[800],
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18
+                    ),
+                  ),
+                ],
+              ),
+            )
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget MainImage(BuildContext context) {
+    return Scaffold(
+      body: Center(
+
+      )
+    );
+  }
+}
+
+
+
